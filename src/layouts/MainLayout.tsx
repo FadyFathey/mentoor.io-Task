b07014909be757354515ce8aadd0f3f8c1646b8e
devfadyfathy <117510974+FadyFathey@users.noreply.github.com>
2024-06-07 19:36:31 +0300

    Fix type errors in MainLayout component

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,24 +1,2 @@
-import React, { useState } from "react";
-import AddTodo from "../components/addTodo/AddTodo";
-import Header from "../components/header/Header";
-import Tasks from "../components/tasks/Tasks";
-import CreatedTask from "../components/util/CreatedTask";
-
-const MainLayout = () => {
-  const [editIndex, setEditIndex] = useState<number | null>(null);
-
-  const handleEdit = (index: number) => {
-    setEditIndex(index); // Set the index of the task being edited
-  };
-
-  return (
-    <div>
-      <Header />
-      <AddTodo editIndex={editIndex} setEditIndex={setEditIndex} />
-      <CreatedTask />
-      <Tasks handleEdit={handleEdit} />
-    </div>
-  );
-};
 
 export default MainLayout;
